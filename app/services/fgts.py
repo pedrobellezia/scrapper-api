c8a1abce4acfc9978d7c48b506560939aeaf2fcf
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-04-06 16:32:34 -0300

    adicionar sleep de 1 segundo para esperar carregamento da página

--- app/services/fgts.py
+++ app/services/fgts.py
@@ -1,19 +1,20 @@
 from playwright.async_api import Page, BrowserContext, TimeoutError as PlaywrightTimeout
 from app.config import logger
 from app.exceptions import ScrapError
+import asyncio
 
 
 class Fgts:
-    URL = "https://consulta-crf.caixa.gov.br/consultacrf/pages/consultaEmpregador.jsf"
-
     @staticmethod
     async def execute_scrap(page: Page, context: BrowserContext, cnpj: str):
         try:
             logger.info(f"Starting FGTS scrape for CNPJ: {cnpj}")
 
-            raise Exception
-
-            await page.goto(Fgts.URL, wait_until="domcontentloaded", timeout=30_000)
+            await page.goto(
+                "https://consulta-crf.caixa.gov.br/consultacrf/pages/consultaEmpregador.jsf",
+                wait_until="domcontentloaded",
+                timeout=30_000,
+            )
 
             await page.locator("//input[@id='mainForm:txtInscricao1']").fill(cnpj)
 
@@ -28,6 +29,8 @@ class Fgts:
             await page.locator("//input[@id='mainForm:btnVisualizar']").click()
 
             await page.wait_for_load_state("networkidle", timeout=20_000)
+
+            await asyncio.sleep(1)
 
             pdf_bytes = await page.pdf()
 
